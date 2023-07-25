6d94bf24a5de892986e780d300c751501710fa62
iscander-b10 <alexbolbychev@gmail.com>
2023-07-26 00:56:59 +0300

    add toogleModal function

--- js/onloadScript.js
+++ js/onloadScript.js
@@ -1,10 +1,16 @@
 import {pasteText} from './pasteTextInBlock.js';
 import {contractOffer} from '../data/contractOffer.js';
 import {privacyPolicy} from '../data/privacyPolicy.js';
+import {toogleModal} from '../js/toogleModal.js';
 
 function onload(){
     pasteText('confidentialityText', privacyPolicy);
     pasteText('offerText', contractOffer);
+    const offerButton = document.getElementById("contractOffer");
+    const modalContainer = document.getElementById("contractOffer__wrapper");
+    offerButton.addEventListener("click", () => toogleModal("contractOffer__wrapper"))
+    const closeOffer = document.getElementById("closeOffer");
+    closeOffer.addEventListener("click", () => toogleModal("contractOffer__wrapper"))
 }
 
 window.onload = onload;
