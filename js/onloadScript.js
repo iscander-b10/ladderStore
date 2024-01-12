2489d941efee6d448d3acc74e5b2a14cbd1dca89
Ivan <i.sukhachev@atlas.online>
2024-01-12 21:40:27 +0300

    add check location in onload

--- js/onloadScript.js
+++ js/onloadScript.js
@@ -1,35 +1,39 @@
-import {scrollOfStairs} from '../js/scrollOfStairs.js';
-import {checkOrderForm} from '../js/orderForm.js';
-import {toggleModalMainPage} from './toggleModalMainPage.js';
-import {toggleModalAboutCompanyPage} from '../js/toggleModalAboutCompanyPage.js';
+import { scrollOfStairs } from "../js/scrollOfStairs.js";
+import { checkOrderForm } from "../js/orderForm.js";
+import { toggleModalMainPage } from "./toggleModalMainPage.js";
+import { toggleModalAboutCompanyPage } from "../js/toggleModalAboutCompanyPage.js";
 
-function onload(){
-    toggleModalMainPage();
-    toggleModalAboutCompanyPage();
-    
-    // const nextOne = document.getElementById("nextOne");
-    // nextOne.addEventListener("click", () => {
-    //     document.getElementById("choiceOfStairs__container").classList.remove("active");
-    //     document.getElementById("sizeOfStairs").classList.add("active"); 
-    // });
+function onload() {
+    if (location.pathname.includes("mainPage")) {
+        toggleModalMainPage();
 
-    // findModalWindow("closeStepTwo", "sizeOfStairs");
+        // const nextOne = document.getElementById("nextOne");
+        // nextOne.addEventListener("click", () => {
+        //     document
+        //         .getElementById("choiceOfStairs__container")
+        //         .classList.remove("active");
+        //     document.getElementById("sizeOfStairs").classList.add("active");
+        // });
 
-    // findModalWindow("closeFacing", "facing");
-    
-    // const nextThree = document.getElementById("calculate");
-    // nextThree.addEventListener("click", () => {
-    //     document.getElementById("facing").classList.remove("active");
-    //     document.getElementById("finish").classList.add("active"); 
-    // });
-    
-    // findModalWindow("closeFinish", "finish");
+        // findModalWindow("closeStepTwo", "sizeOfStairs");
 
-    scrollOfStairs();
-    
-    checkOrderForm();
+        // findModalWindow("closeFacing", "facing");
+
+        // const nextThree = document.getElementById("calculate");
+        // nextThree.addEventListener("click", () => {
+        //     document.getElementById("facing").classList.remove("active");
+        //     document.getElementById("finish").classList.add("active");
+        // });
+
+        // findModalWindow("closeFinish", "finish");
+        
+        scrollOfStairs();
+        checkOrderForm();
+    }
+
+    if (location.pathname.includes("aboutCompanyPage")) {
+        toggleModalAboutCompanyPage();
+    }
 }
 
 window.onload = onload;
-
-
